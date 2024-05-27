62cb5a4c9090cf346ff6605dcab1e3695c751ad5
deco-L <scotchwitchbroom@gmail.com>
2024-05-27 13:09:13 +0900

    create file

---
+++ ex00/include/ScalarConverter.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
+/*   Updated: 2024/05/27 13:00:13 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <cstdlib>
+
+class ScalarConverter
+{
+};
+
+#endif
---
+++ ex01/include/Serializer.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
+/*   Updated: 2024/05/27 13:00:09 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <cstdlib>
+
+class Serializer
+{
+};
+
+#endif
